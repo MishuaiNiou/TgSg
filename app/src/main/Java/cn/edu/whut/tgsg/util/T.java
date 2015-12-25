192817b339eaee112a5d571b6c88a3f51962ab46
mishuainiou <1121919964@qq.com>
2015-12-25 12:34:53 +0800

    Toast工具类优化

--- app/src/main/Java/cn/edu/whut/tgsg/util/T.java
+++ app/src/main/Java/cn/edu/whut/tgsg/util/T.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
  */
 public class T {
 
+    private static Toast toast;
+
     /**
      * 显示Toast(短时间)
      *
@@ -19,4 +21,39 @@ public class T {
     public static void show(Context context, CharSequence message) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
     }
+
+    /**
+     * Toast工具类优化
+     *
+     * @param context
+     * @param text
+     * @param duration
+     */
+    public static void showToast(Context context, CharSequence text, int duration) {
+        if (toast == null) {
+            toast = Toast.makeText(context, text, duration);
+        } else {
+            toast.setText(text);
+            toast.setDuration(duration);
+        }
+        toast.show();
+    }
+
+    /**
+     * Toast工具类优化
+     *
+     * @param context
+     * @param text
+     */
+    public static void showToast(Context context, CharSequence text) {
+        if (toast == null) {
+            toast = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(text);
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        toast.show();
+    }
+
+
 }
